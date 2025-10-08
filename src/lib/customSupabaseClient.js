e841fc9d4f0d15d0fae69eaa78e774acec1d248e
lopezdav-code <lopez.dav@gmail.com>
2025-10-08 14:03:13 +0200

    Gestion de la BDD avec des variables d'environnement

--- src/lib/customSupabaseClient.js
+++ src/lib/customSupabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://hvugiirundpxynozxxnd.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh2dWdpaXJ1bmRweHlub3p4eG5kIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkyNTM0NjYsImV4cCI6MjA3NDgyOTQ2Nn0.aEH6Lp5B8qceXfY-uLIPJJYoE-ecUq78beLBcyD-bww';
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
